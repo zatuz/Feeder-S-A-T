92ebdbd5affd67fea758a09fe9bf52971ea3c143
zatuz <leo_nesios@hotmail.com>
2019-03-30 07:29:43 -0600

    creado con script sh
    se crea script para automatizar
    usuario
    agregar dependencias + ansible
    e instalador de docker.sh

---
+++ 1.-newuser.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#variables de user an password
+readonly user="mi-usuario"
+readonly pass="my-password"
+
+useradd -m "$user"
+usermod -aG sudo "$user"
+usermod -aG "$user" "$user"
+echo "$pass" | passwd "$user" --stdin
+chsh -s /bin/bash "$user"
+su "$user"
+
+## opcion 1 de variables para user an password
+#read -p "Enter your username: " username
+#read -sp "Enter your password: " password
+## si no funciona la variable username y password usar este script para el password
+#!/bin/bash
+# Read Password
+#echo -n Password: 
+#read -s password
+#echo
+# Run Command
+#echo $password
+
+## script para el pasword
+#echo thePassword | passwd $user --stdin
+
---
+++ 3.- dep-1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+## creditos a cloudbox que tome ideas de su archivo dep.sh cloudbox.works	
+## Disable IPv6
+if [ -f /etc/sysctl.d/99-sysctl.conf ]; then
+    grep -q -F 'net.ipv6.conf.all.disable_ipv6 = 1' /etc/sysctl.d/99-sysctl.conf || \
+        echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.d/99-sysctl.conf
+    grep -q -F 'net.ipv6.conf.default.disable_ipv6 = 1' /etc/sysctl.d/99-sysctl.conf || \
+        echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.d/99-sysctl.conf
+    grep -q -F 'net.ipv6.conf.lo.disable_ipv6 = 1' /etc/sysctl.d/99-sysctl.conf || \
+        echo 'net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.d/99-sysctl.conf
+    sysctl -p
+fi
+# Install Pre-Dependencies
+apt-get install -y --reinstall \
+		apt-transport-https \
+		dirmngr \
+		software-properties-common \
+## Add APT repos
+add-apt-repository main
+add-apt-repository contrib
+add-apt-repository non-free
+## agrega repositorios ansible
+sudo sh -c 'echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list'
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+# instalamos paquetes apt
+apt update
+apt install -y \
+	build-essential \
+	curl \
+	git\
+	module-assistant \
+	nano \
+	libssl-dev \
+    libffi-dev \
+    python3-dev \
+    python3-pip \
+    python-dev \
+    python-pip
+m-a prepare
+apt upgrade -y
+apt dist-upgrade -y
