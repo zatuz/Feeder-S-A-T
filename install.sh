76d269b7675bff7bcde64cf275a3795e56e6b930
zatuz <leo_nesios@hotmail.com>
2019-03-31 08:40:51 -0600

    unos arreglos
    unos arreglos

--- install.sh
+++ install.sh
@@ -6,16 +6,16 @@ read user
 readonly dep1="script/3.-dep-1.sh"
 readonly docker="script/4.-installdocker.sh"
 export 	VAR1="$user"
-if grep -qi "^$user:" /etc/passwd ; then
+if [ grep -qi "^$user:" /etc/passwd ]; then
 	. "script/1.1-olduser.sh"
 else
 	. "script/1.-newuser.sh"	
 fi
-echo "%$user ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/$user
+echo "\%$user ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/$user
 cp -r ../Feeder-S-A-T /home/$user/
-chown -R "$user":"$user" /home/$user/Feeder-S-A-T
-su "$user"
-cd ~/Feeder-S-A-T
+chown -R $user:$user  /home/$user/Feeder-S-A-T
+su $user
+cd  ~/Feeder-S-A-T
 sudo ./"$dep1"
 sudo ./"$docker"
 
--- script/4.-installdocker.sh
+++ script/4.-installdocker.sh
@@ -4,21 +4,23 @@ apt-get install -y \
 		ca-certificates \
 		gnupg2 
 ## agregamos claves docker
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+	
 ##agrego repositorios docker
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
-   stable"  
+   stable"
 ## actualizo e instalo docker
+apt update 
 apt-get install -y \
 	docker-ce \
 	docker-ce-cli \
 	containerd.io \
-	docker.io nodejs \
+	nodejs \
 	npm 
-pip install docker-compose==1.9.0
-pip install docker-py
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+pip install docker-py	
 npm install npm --global
 ## se agrega $user a grupo docker
 usermod -aG docker "$VAR1"
