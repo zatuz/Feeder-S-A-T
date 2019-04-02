bbf096e138e26beb05b4cd64c74043cadbb4dec8
zatuz <leo_nesios@hotmail.com>
2019-04-02 01:22:54 -0600

    acomodo y renombrado
    se utilizo la herramientas
    shellcheck y shellharden para verificar errores y se renombraron los script secundarios como 1newuser, 2olduser, 3dep y 4docker para mejor trabajo con el instalador

--- install.sh
+++ install.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
+set -o pipefail
 #iniciamos el primer script para crear usuario
+#readonly dep1="script/3.-dep-1.sh"
+#readonly docker="script/4.-installdocker.sh"
+##variables para usuario
 echo "si ingresas un usuario que no existe se acreara el usuario"
 echo -n "que usuario quieres usar? user:"
-read user
-readonly dep1="script/3.-dep-1.sh"
-readonly docker="script/4.-installdocker.sh"
-export 	VAR1="$user"
-if [ grep -qi "^$user:" /etc/passwd ]; then
-	. "script/1.1-olduser.sh"
+read -r  user
+export  VAR1="$user"
+# verifica usuario para crear o modificar
+if   " grep -qi ^$user: /etc/passwd " ; then
+        . script/2olduser.sh
 else
-	. "script/1.-newuser.sh"	
-fi
-echo "\%$user ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/$user
-cp -r ../Feeder-S-A-T /home/$user/
-chown -R $user:$user  /home/$user/Feeder-S-A-T
-su $user
-cd  ~/Feeder-S-A-T
-sudo ./"$dep1"
-sudo ./"$docker"
-
+        . script/1newuser.sh
+ fi
+#crea archivo sudoers
+echo "%$user ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/"$user"
+cp -r /home/zatuz/Feeder-S-A-T /home/"$user"/ > /dev/null 2>&1
+chown -R "$user":"$user"  /home/"$user"/Feeder-S-A-T
+su "$user" -c
+cd /home/"$user"/Feeder-S-A-T/
+. "script/3dep.sh"
+. "script/4docker.sh"
--- script/4docker.sh
+++ script/4docker.sh
@@ -2,25 +2,23 @@
 ## instalamos lo requisitos etc
 apt-get install -y \
 		ca-certificates \
-		gnupg2 
+		gnupg2
 ## agregamos claves docker
-	
+
 ##agrego repositorios docker
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
+	"deb [arch=amd64] https://download.docker.com/linux/debian (lsb_release -cs) stable"
 ## actualizo e instalo docker
-apt update 
+apt update
 apt-get install -y \
 	docker-ce \
 	docker-ce-cli \
 	containerd.io \
 	nodejs \
-	npm 
+	npm
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-pip install docker-py	
+pip install docker-py
 npm install npm --global
 ## se agrega $user a grupo docker
 usermod -aG docker "$VAR1"
