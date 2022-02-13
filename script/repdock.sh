7af79d28ed47961abf899279e3e118552f668c74
zatuz <39281593+zatuz@users.noreply.github.com>
2022-02-13 13:34:20 -0600

    agregando nuevo repo docker

--- script/repdock.sh
+++ script/repdock.sh
@@ -13,9 +13,8 @@ if grep -qi 'download.docker.com': /etc/apt/sources.list; then
        echo "hola ya tienes contenedores docker "
 else
     echo "pasaremos a inestalar repositorios docker"
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-    add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/debian \
-    $(lsb_release -cs) \
-    stable"
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 fi
