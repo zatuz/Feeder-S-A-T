861444d2a970e5daa53eb8450e69df218a7d9bb5
zatuz <39281593+zatuz@users.noreply.github.com>
2022-02-13 02:38:11 -0600

    setup repo V16

--- script/repnode.sh
+++ script/repnode.sh
@@ -4,6 +4,6 @@ set -o pipefail
 if [ ! -d /etc/apt/sources.list.d/nodesource.list ]; then
 echo
 echo "instalando repo nodejs"
-curl -sL https://deb.nodesource.com/setup_11.x | bash -
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
 apt-get install -y nodejs
 fi
