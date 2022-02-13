6b093130dabeb78a4f831f6df40d87c0dc40696b
zatuz <39281593+zatuz@users.noreply.github.com>
2022-02-13 14:24:36 -0600

    Update compose.sh

--- script/compose.sh
+++ script/compose.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 set -o pipefail
+##comprobamos que existe la carpeta de plugins de docker 
+
 ##se instalara complemento docker "docker compose"
 if [ -e "/usr/local/bin/docker-compose" ];then
     echo "docker-compose installed"
 else
-    curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /tmp/docker-compose &&
-    mv /tmp/docker-compose /usr/local/bin/docker-compose &&
-    chmod +x /usr/local/bin/docker-compose
+     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+&&
+    mv /tmp/docker-compose /usr/local/bin/docker-compose && \
+    chmod +x /usr/local/bin/docker-compose 
     ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+fi
+if [[ -d ~/.docker/cli-plugins/ ]];then
+    echo "ya esta creada docker cli plugins"
+        if [ -e "~/.docker/cli-plugins/docker-compose" ];then
+                echo "ya existe docker compose en la carpeta plugins"
+            else
+                curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+                echo "docker compose v2 instalado correctamente" 
+                chmod +x ~/.docker/cli-plugins/docker-compose
+        fi    
+else
+        mkdir -p ~/.docker/cli-plugins/
+        curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+        echo "docker compose v2 instalado correctamente" 
+        chmod +x ~/.docker/cli-plugins/docker-compose
 fi
