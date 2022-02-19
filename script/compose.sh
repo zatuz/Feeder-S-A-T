6ba39b23f927e404d06e359f8976233bb56d45a6
zatuz <39281593+zatuz@users.noreply.github.com>
2022-02-19 02:41:28 -0600

    modificando para docker compose 1 y 2
    se modifica escript para verificar la instalacion de docker compose v1 o v2 y  si no se encuentra instalado te pregunta cual version quieres intalar

--- script/compose.sh
+++ script/compose.sh
@@ -3,27 +3,24 @@ set -o pipefail
 ##comprobamos que existe la carpeta de plugins de docker 
 
 ##se instalara complemento docker "docker compose"
-if [ -e "/usr/local/bin/docker-compose" ];then
+if [ -e "/usr/local/bin/docker-compose" ]; then
     echo "docker-compose installed"
-else
-     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
-&&
-    mv /tmp/docker-compose /usr/local/bin/docker-compose && \
-    chmod +x /usr/local/bin/docker-compose 
-    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-fi
-if [[ -d ~/.docker/cli-plugins/ ]];then
-    echo "ya esta creada docker cli plugins"
-        if [ -e "~/.docker/cli-plugins/docker-compose" ];then
-                echo "ya existe docker compose en la carpeta plugins"
-            else
-                curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-                echo "docker compose v2 instalado correctamente" 
-                chmod +x ~/.docker/cli-plugins/docker-compose
-        fi    
-else
-        mkdir -p ~/.docker/cli-plugins/
-        curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-        echo "docker compose v2 instalado correctamente" 
-        chmod +x ~/.docker/cli-plugins/docker-compose
+ elif [ -e "~/.docker/cli-plugins/docker-compose" ]; then
+    echo "docker-compose installed"
+ else
+    echo "docker-compose no instalado"
+    echo "quieres instalar la version 1 de docker compose escribe 1 y enter"
+    echo "o si quieres instalar la version 2 de docker-compose escribe 2 y enter"
+    read -r COMPOSER
+    if [ "$COMPOSER" == "1" ]; then
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o "/usr/local/bin/docker-compose"
+        sudo chmod +x "/usr/local/bin/docker-compose"
+        sudo ln -s "/usr/local/bin/docker-compose" "/usr/bin/docker-compose"
+        echo "docker compose v1 instalado correctamente"
+     elif [ "$COMPOSER" == "2" ]; then
+        mkdir -p "~/.docker/cli-plugins/"
+        curl -SL "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64" -o "~/.docker/cli-plugins/docker-compose"
+        chmod +x "~/.docker/cli-plugins/docker-compose"
+        echo "docker compose v2 instalado correctamente"
+    fi
 fi
