2bc5621ce08a4906a0c55fdddad188f2befa4aeb
zatuz <39281593+zatuz@users.noreply.github.com>
2022-03-20 08:12:30 -0600

    Update repoficial.sh

--- script/repoficial.sh
+++ script/repoficial.sh
@@ -8,12 +8,12 @@ if [ -f  /etc/apt/sources.list.d/backports.list ]; then
         echo "existe archivo de repo backports"
         grep -q -F "deb http://ftp.debian.org/debian $(lsb_release -cs)-backports main" /etc/apt/sources.list.d/backports.list && \
                 echo "existe repo deb" || \
-                echo "#deb http://ftp.debian.org/debian  $(lsb_release -cs)-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list.d/backports.list
+                echo "#deb http://ftp.debian.org/debian  $(lsb_release -cs)-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list.d/backports.list >/dev/null
         grep -q -F "deb-src http://ftp.debian.org/debian  $(lsb_release -cs)-backports main" /etc/apt/sources.list.d/backports.list && \
                 echo "existe repo deb-src" || \
-                echo "#deb-src http://ftp.debian.org/debian  $(lsb_release -cs)-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list.d/backports.list
+                echo "#deb-src http://ftp.debian.org/debian  $(lsb_release -cs)-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list.d/backports.list >/dev/null
 else
         sudo touch /etc/apt/sources.list.d/backports.list
-        echo "#deb http://ftp.debian.org/debian  $(lsb_release -cs)-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list.d/backports.list
-        echo "#deb-src http://ftp.debian.org/debian  $(lsb_release -cs)-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list.d/backports.list
+        echo "#deb http://ftp.debian.org/debian  $(lsb_release -cs)-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list.d/backports.list >/dev/null
+        echo "#deb-src http://ftp.debian.org/debian  $(lsb_release -cs)-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list.d/backports.list >/dev/null
 fi
