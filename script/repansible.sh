14f4015b6ba7a0fb937b43f371cbfe89b47d64c3
zatuz <39281593+zatuz@users.noreply.github.com>
2022-03-20 08:14:09 -0600

    Update repansible.sh

--- script/repansible.sh
+++ script/repansible.sh
@@ -5,7 +5,7 @@ if [ -f /etc/apt/sources.list.d/ansible.list ] && grep -qi 'http://ppa.launchpad
    echo "tenemos ansible"
 else
     echo "instalando ansible"
-    echo '#deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' > /etc/apt/sources.list.d/ansible.list
+    echo '#deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' > /etc/apt/sources.list.d/ansible.list >/dev/null
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-sudo apt-get update
+sudo apt-get update >/dev/null
 fi
