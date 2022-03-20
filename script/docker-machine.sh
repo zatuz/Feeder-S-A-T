6df2112712603e8f84b2a35b910b4ca634fb0fa2
zatuz <39281593+zatuz@users.noreply.github.com>
2022-03-20 05:29:11 -0600

    Update docker-machine.sh

--- script/docker-machine.sh
+++ script/docker-machine.sh
@@ -5,7 +5,7 @@ if [ -e "/usr/local/bin/docker-machine" ];then
     echo "docker machine all installed"
 else
     base=https://github.com/docker/machine/releases/download/v0.16.0 &&
-    curl -L $base/docker-machine-"$(uname -s)"-"$(uname -m)" >/tmp/docker-machine &&
+    curl -L $base/docker-machine-"$(uname -s)"-"$(uname -m)" > /tmp/docker-machine &&
     sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
     chmod +x /usr/local/bin/docker-machine
 fi
