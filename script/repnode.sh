a0e80dbdf18cb1a8f872543d483be3ae7d38aa6c
zatuz <39281593+zatuz@users.noreply.github.com>
2022-03-29 02:26:11 -0600

    Update repnode.sh

--- script/repnode.sh
+++ script/repnode.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -o pipefail
 ##repositorios para node js y npm
-if [ ! -d /etc/apt/sources.list.d/nodesource.list ]; then
-echo
-echo "instalando repo nodejs"
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
-apt-get install -y nodejs
+if [ -f /etc/apt/sources.list.d/nodesource.list ] && grep -qi 'deb.nodesource.com' /etc/apt/sources.list.d/nodesource.list; then
+  echo "repositorio nodejs instalado"
+else 
+  echo
+  echo "instalando repo nodejs"
+  curl -sL https://deb.nodesource.com/setup_16.x | bash -
+  apt-get install -y nodejs
 fi
