2e009575eec3bb7c9d0faba0415d51181038eeae
zatuz <39281593+zatuz@users.noreply.github.com>
2022-03-29 02:28:01 -0600

    Update repoficial.sh

--- script/repoficial.sh
+++ script/repoficial.sh
@@ -4,9 +4,16 @@ set -o pipefail
 add-apt-repository main
 add-apt-repository contrib
 add-apt-repository non-free
-if [ -f  /etc/apt/sources.list ]; then
-    grep -q -F 'deb http://ftp.debian.org/debian  $(lsb_release -cs)-backports main' /etc/apt/sources.list || \
-        echo '#deb http://ftp.debian.org/debian  $(lsb_release -cs)-backports main contrib non-free' >> /etc/apt/sources.list
-    grep -q -F 'deb-src http://ftp.debian.org/debian  $(lsb_release -cs)-backports main' /etc/apt/sources.list || \
-        echo '#deb-src http://ftp.debian.org/debian  $(lsb_release -cs)-backports main contrib non-free' >> /etc/apt/sources.list
+if [ -f  /etc/apt/sources.list.d/backports.list ]; then
+        echo "existe archivo de repo backports"
+        grep -q -F "deb http://ftp.debian.org/debian $(lsb_release -cs)-backports main" /etc/apt/sources.list.d/backports.list && \
+                echo "existe repo deb" || \
+                echo "#deb http://ftp.debian.org/debian  $(lsb_release -cs)-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list.d/backports.list >/dev/null
+        grep -q -F "deb-src http://ftp.debian.org/debian  $(lsb_release -cs)-backports main" /etc/apt/sources.list.d/backports.list && \
+                echo "existe repo deb-src" || \
+                echo "#deb-src http://ftp.debian.org/debian  $(lsb_release -cs)-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list.d/backports.list >/dev/null
+else
+        sudo touch /etc/apt/sources.list.d/backports.list
+        echo "#deb http://ftp.debian.org/debian  $(lsb_release -cs)-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list.d/backports.list >/dev/null
+        echo "#deb-src http://ftp.debian.org/debian  $(lsb_release -cs)-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list.d/backports.list >/dev/null
 fi
